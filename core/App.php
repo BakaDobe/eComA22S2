72b9a75f95cd0ceba153f9c2365c4050e6482853
BakaDobe <anthoniuspowellus@gmail.com>
2022-08-25 16:11:24 -0400

    Added all the components

---
+++ core/App.php
@@ -0,0 +1,9 @@
+<?php
+	
+	namespace \app\core;
+
+	class App{
+		public function __constraint(){
+			echo "A new App is born";
+		}
+	}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+	//include();
+	//include_once();
+	//require();
+	//require_once();
+	//require_once('app\coreApp.php');
+	echo 'require from index.php';
